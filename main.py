601f1906a73e899702cd454b35a68a2e85f11ee2
Valter Wagner Zethrin <valter.wagner.zethrin@ri.se>
2023-05-28 14:43:49 +0200

    * Added main file with task scheduler * Added database connection

---
+++ main.py
@@ -0,0 +1,20 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def print_hi():
+    print('Hi')
+
+def main():
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    
+    scheduler.add_job(print_hi, 'interval', minutes=1)
+    
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        scheduler.shutdown(wait=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/weather_station/database/connection.py
@@ -0,0 +1,21 @@
+import psycopg2
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+def get_connection():
+    load_dotenv(Path(__file__).parent.parent.parent.parent / ".env")
+
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+
+    connection = psycopg2.connect(
+        host=db_host, port=db_port, database=db_name, user=db_user,
+        password=db_password
+    )
+
+    return connection
