2cc0b81cf486dec7e9113dc5994019d673ddcf2c
Valter Wagner Zethrin <valter.wagner.zethrin@ri.se>
2023-05-28 22:20:47 +0200

    * Added pytest dependency

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 def print_hi():
     print('Hi')
+
 
 def main():
     scheduler = BackgroundScheduler()
     scheduler.start()
-    
+
     scheduler.add_job(print_hi, 'interval', minutes=1)
-    
+
     try:
         while True:
             pass
